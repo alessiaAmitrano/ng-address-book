9685b5eed198cdb313b9289fcff04c9a847d8730
Alessia Amitrano <alessiaa@eu.square-enix.com>
2023-01-15 18:10:03 +0000

    change full name into first name and last name

--- src/app/components/containers/address-book/address-book.component.ts
+++ src/app/components/containers/address-book/address-book.component.ts
@@ -1,11 +1,17 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
+import { AddressItem } from 'src/app/core/models';
 
 @Component({
   selector: 'app-address-book-container',
   templateUrl: './address-book.component.html',
   styleUrls: ['./address-book.component.scss'],
 })
-export class AddressBookComponent {
+export class AddressBookComponent implements OnInit {
   @Input() addressItems?: Observable<any[]>;
+  filteredItems: AddressItem[] = [];
+
+  ngOnInit() {
+    this.addressItems?.subscribe(value => this.filteredItems = value);
+  }
 }
--- src/app/core/models/address-item.model.ts
+++ src/app/core/models/address-item.model.ts
@@ -1,4 +1,5 @@
 export interface AddressItem {
-  full_name: string;
+  first_name: string;
+  last_name: string;
   phone_number?: string;
 }
